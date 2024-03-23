121e4297a112efa7648c834ec9579578a630b165
devjrios <jrios@tuta.io>
2024-03-23 09:18:02 -0500

    Adds firmware entry to bootloader

--- nixos/common/bootloader.nix
+++ nixos/common/bootloader.nix
@@ -5,4 +5,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 2;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.extraEntries = {
+    "firmware_update.conf" = ''
+    title    Linux Firmware Updater
+    efi    /EFI/nixos/fwupdx64.efi
+    '';
+  };
 }
