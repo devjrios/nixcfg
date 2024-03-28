559b71421497fca80816a5740c9b6a0edf5e501f
devjrios <jrios@tuta.io>
2024-03-28 15:39:10 -0500

    testing kernel unstable branch, bump nvidia

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -1,6 +1,6 @@
-{ pkgs, ... }:
+{ pkgs-unstable, ... }:
 {
   # boot.kernelParams = [ "nohibernate" ];
   boot.tmp.cleanOnBoot = true;
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernelPackages = pkgs-unstable.linuxPackages_xanmod;
 }
