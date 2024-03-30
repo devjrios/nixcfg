5e49ebe009989ea68da1d526b6e31e96efc64f2a
devjrios <jrios@tuta.io>
2024-03-29 20:23:20 -0500

    prefer modules from stable branch, to many lockups

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -1,6 +1,6 @@
-{ pkgs-unstable, ... }:
+{ pkgs, ... }:
 {
   # boot.kernelParams = [ "nohibernate" ];
   boot.tmp.cleanOnBoot = true;
-  boot.kernelPackages = pkgs-unstable.linuxPackages_xanmod;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod;
 }
