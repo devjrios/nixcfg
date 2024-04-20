6995315109ce78d01416a749d74c0e64ed68c783
devjrios <jrios@tuta.io>
2024-04-19 22:30:38 -0500

    enables zswap

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   # boot.kernelParams = [ "nohibernate" ];
+  boot.kernelParams = [ "zswap.enabled=1" ];
   boot.tmp.cleanOnBoot = true;
   boot.kernelPackages = pkgs.linuxPackages_xanmod;
 }
