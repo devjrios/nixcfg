e47d0d1fe4c39d7d18e01c9cbe0cdb91d737b085
devjrios <jrios@tuta.io>
2024-08-26 10:49:49 -0500

    update kernel and nvidia version

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -4,5 +4,6 @@
   # boot.kernelParams = [ "zswap.enabled=1" ];
   boot.tmp.cleanOnBoot = true;
   # boot.kernelPackages = pkgs.linuxPackages_xanmod;
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
+  # boot.kernelPackages = pkgs.linuxPackages_6_1;
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
 }
