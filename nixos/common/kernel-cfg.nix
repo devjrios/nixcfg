fd2522d94dd37856d8223c1ad95b4872ce80b56c
devjrios <jrios@tuta.io>
2024-08-27 06:22:08 -0500

    allow xanmod again

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -3,7 +3,7 @@
   # boot.kernelParams = [ "nohibernate" ];
   # boot.kernelParams = [ "zswap.enabled=1" ];
   boot.tmp.cleanOnBoot = true;
-  # boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod;
   # boot.kernelPackages = pkgs.linuxPackages_6_1;
-  boot.kernelPackages = pkgs.linuxPackages_6_6;
+  # boot.kernelPackages = pkgs.linuxPackages_6_6;
 }
