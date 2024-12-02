7daa690889d6f86dd18df2000a8e58a9d6f1fb74
devjrios <jrios@tuta.io>
2024-12-02 12:02:35 -0500

    reintroduce nerd symbols

--- nixos/common/desktop.nix
+++ nixos/common/desktop.nix
@@ -72,7 +72,7 @@
     packages = with pkgs; [
       gyre-fonts
       noto-fonts-color-emoji
-      # (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" "VictorMono" ]; })
+      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" "VictorMono" ]; })
       # nerd-fonts.symbols-only
       # nerd-fonts.victor-mono
       # nerd-fonts.ubuntu-mono
