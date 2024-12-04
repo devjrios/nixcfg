c128e3a55dcdd53a32a683cd9207ff38c5de5ab1
devjrios <jrios@tuta.io>
2024-12-03 19:22:36 -0500

    enable msbc again

--- nixos/common/desktop.nix
+++ nixos/common/desktop.nix
@@ -59,7 +59,7 @@
       "10-bluez" = {
         "monitor.bluez.properties" = {
           "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = false;
+          "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
           "bluez5.hfphsp-backend" = "native";
         };
