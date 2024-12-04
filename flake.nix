1101625f2734b3d88cf74778556fa10586fcb19c
devjrios <jrios@tuta.io>
2024-12-03 21:00:56 -0500

    got rid of chaotic, mesa-git didn't work

--- flake.nix
+++ flake.nix
@@ -5,10 +5,9 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, chaotic, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -18,7 +17,6 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./nixos
-          chaotic.nixosModules.default
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
--- nixos/common/gfx-nvidia.nix
+++ nixos/common/gfx-nvidia.nix
@@ -4,8 +4,6 @@
 
     services.xserver.videoDrivers = [ "nvidia" ];
 
-    chaotic.mesa-git.enable = true;
-
     hardware.graphics.enable = true;
 
     hardware.nvidia = {
