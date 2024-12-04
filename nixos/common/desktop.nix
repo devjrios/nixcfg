5cebbc08a60bfec4c56b09e254cffea7776fcdf4
devjrios <jrios@tuta.io>
2024-12-03 19:40:43 -0500

    get rid of redundant entry in wireplumber

--- nixos/common/desktop.nix
+++ nixos/common/desktop.nix
@@ -55,23 +55,6 @@
       };
     };
     wireplumber.enable = true;
-    wireplumber.extraConfig = {
-      "10-bluez" = {
-        "monitor.bluez.properties" = {
-          "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = true;
-          "bluez5.enable-hw-volume" = true;
-          "bluez5.hfphsp-backend" = "native";
-        };
-      };
-
-      "11-bluetooth-policy" = {
-        "wireplumber.settings" = {
-          "bluetooth.autoswitch-to-headset-profile" = false;
-        };
-      };
-
-    };
   };
   nixpkgs.config.pulseaudio = true;
   nixpkgs.config.pipewire = true;
