8ab879de3822b12c8088425fcc14034345a7abff
devjrios <jrios@tuta.io>
2024-12-05 23:35:15 -0500

    experiment with lix-module

--- flake.nix
+++ flake.nix
@@ -5,9 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, lix-module, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -24,6 +28,7 @@
             home-manager.users.jrios.imports = [ ./home ];
             home-manager.extraSpecialArgs = { inherit inputs; };
           }
+          lix-module.nixosModules.default
         ];
       };
     };
