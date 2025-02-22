af7b19090a522b067b26ea7b9b30e87607690652
devjrios <july.rios@protonmail.com>
2025-02-22 12:59:46 -0500

    noissue; allow udp chromecast

--- nixos/default.nix
+++ nixos/default.nix
@@ -17,6 +17,11 @@
 
   networking.hostName = "nixos";
   # networking.firewall.enable = false;
+  networking.firewall = {
+    allowedUDPPorts = [ 5353 ]; # For device discovery
+    allowedUDPPortRanges = [{ from = 32768; to = 61000; }]; # For Streaming
+    allowedTCPPorts = [ 8010 ];  # For gnomecast server
+  };
   networking.networkmanager = {
     enable = true;
     wifi.powersave = false;
