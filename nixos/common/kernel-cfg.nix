8d72a630349de5f2e5c9df9dfc181e0b9bea5838
devjrios <july.rios@protonmail.com>
2025-05-22 23:15:39 -0500

    test 6.6 with tkg patches

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -1,8 +1,23 @@
 {pkgs, ...}: {
   boot.tmp.cleanOnBoot = true;
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
-  # boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-  # boot.kernelPackages = pkgs.linuxPackages_6_6;
-  # boot.kernelPackages = pkgs.linuxPackages_6_12;
-  # boot.kernelPackages = pkgs.linuxPackages_6_1;
+  # boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
+  boot.kernelPatches = [
+    {
+      name = "bore";
+      patch = ./tkg-patches/0001-bore.patch;
+    }
+    {
+      name = "clear";
+      patch = ./tkg-patches/0002-clear-patches.patch;
+    }
+    {
+      name = "misc";
+      patch = ./tkg-patches/0012-misc-additions.patch;
+    }
+    {
+      name = "o3";
+      patch = ./tkg-patches/0013-optimize_harder_O3.patch;
+    }
+  ];
 }
