8dcea4e85d7f090b33534572c1616db00f75c2da
devjrios <july.rios@protonmail.com>
2025-05-22 19:36:56 -0500

    update nvidia prod driver

--- nixos/common/gfx-nvidia.nix
+++ nixos/common/gfx-nvidia.nix
@@ -13,7 +13,15 @@
   hardware.nvidia-container-toolkit.enable = true;
 
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "570.153.02";
+      sha256_64bit = "sha256-FIiG5PaVdvqPpnFA5uXdblH5Cy7HSmXxp6czTfpd4bY=";
+      sha256_aarch64 = lib.fakeSha256;
+      openSha256 = lib.fakeSha256;
+      settingsSha256 = lib.fakeSha256;
+      persistencedSha256 = lib.fakeSha256;
+    };
 
     open = false;
     modesetting.enable = true;
