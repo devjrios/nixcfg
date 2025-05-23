1f553dd2743eee958f1a302f18c7dd5996114f76
devjrios <july.rios@protonmail.com>
2025-05-22 21:30:43 -0500

    testing lqx kernel

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   boot.tmp.cleanOnBoot = true;
-  boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  # boot.kernelPackages = pkgs.linuxPackages_xanmod;
+  boot.kernelPackages = pkgs.linuxPackages_lqx;
   # boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   # boot.kernelPackages = pkgs.linuxPackages_6_6;
   # boot.kernelPackages = pkgs.linuxPackages_6_12;
--- nixos/default.nix
+++ nixos/default.nix
@@ -6,6 +6,7 @@
   imports = [
     ./common
     ./hardware-configuration.nix
+    ./battery.nix
   ];
 
   users.users.jrios = {
