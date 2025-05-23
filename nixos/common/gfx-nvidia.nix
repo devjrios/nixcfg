41e9145df10cd8ef114984432d3e89d52db6abec
devjrios <july.rios@protonmail.com>
2025-05-23 06:30:01 -0500

    nvidia lower version

--- nixos/common/gfx-nvidia.nix
+++ nixos/common/gfx-nvidia.nix
@@ -15,7 +15,7 @@
   hardware.nvidia = {
 
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.153.02";
+      version = "550.163.01";
       sha256_64bit = "sha256-FIiG5PaVdvqPpnFA5uXdblH5Cy7HSmXxp6czTfpd4bY=";
       sha256_aarch64 = lib.fakeSha256;
       openSha256 = lib.fakeSha256;
