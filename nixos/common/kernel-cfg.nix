ce7aee77b14dd0a3e06c0f8ae6bd39d28d650d42
devjrios <july.rios@protonmail.com>
2025-05-22 21:34:58 -0500

    revert to xanmod

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   boot.tmp.cleanOnBoot = true;
-  # boot.kernelPackages = pkgs.linuxPackages_xanmod;
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod;
   # boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   # boot.kernelPackages = pkgs.linuxPackages_6_6;
   # boot.kernelPackages = pkgs.linuxPackages_6_12;
