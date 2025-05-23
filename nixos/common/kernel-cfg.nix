0af421117dcc79bfda4070853a62015b8239ce44
devjrios <july.rios@protonmail.com>
2025-05-22 21:33:57 -0500

    test with zen

--- nixos/common/kernel-cfg.nix
+++ nixos/common/kernel-cfg.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   boot.tmp.cleanOnBoot = true;
   # boot.kernelPackages = pkgs.linuxPackages_xanmod;
-  boot.kernelPackages = pkgs.linuxPackages_lqx;
+  boot.kernelPackages = pkgs.linuxPackages_zen;
   # boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
   # boot.kernelPackages = pkgs.linuxPackages_6_6;
   # boot.kernelPackages = pkgs.linuxPackages_6_12;
